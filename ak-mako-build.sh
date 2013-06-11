9dc6ee79cac7867b0dcf67bbe5d2241e47b202ad
anarkia1976 <stefano.villa1976@gmail.com>
2013-06-11 12:38:05 +0200

    ak-mako-build.sh: Add Ramdisk XZ Compression

--- ak-mako-build.sh
+++ ak-mako-build.sh
@@ -49,7 +49,8 @@ make -j3 > /dev/null
 rm `echo $MODULES_DIR"/*"`
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
 cd $INIT_DIR
-find . | cpio -o -H newc | gzip -9 > ../initrd.img
+#find . | cpio -o -H newc | gzip -9 > ../initrd.img
+find . | cpio -o -H newc | xz --check=crc32 --lzma2=dict=1MiB > ../initrd.img
 
 cd ../
 cp -vr $ZIMAGE_DIR/zImage .
